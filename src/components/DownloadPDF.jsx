2fafefc4837d861dafeca645eb22edbe4596ce93
Diego Alexander Rivera Acosta <diegoarivera35@gmail.com>
2024-05-16 10:34:33 -0400

    fixxing resume btn styles

--- src/components/DownloadPDF.jsx
+++ src/components/DownloadPDF.jsx
@@ -1,13 +1,15 @@
 
+import { FaFileDownload } from "react-icons/fa";
+
 function DownloadPDF() {
   const pdfUrlEN = "https://images-portfolio-diego-rivera.s3.us-east-2.amazonaws.com/curriculumVitae/EN_WEBDEV_2024.pdf"; 
   const pdfUrlFR = "https://images-portfolio-diego-rivera.s3.us-east-2.amazonaws.com/curriculumVitae/FR_WEBDEV_2024.pdf"; 
 
   return (
     <div className="download-container">
-      <h1>Download My Resume</h1>
-      <a href={pdfUrlEN} download="Diego_Rivera_Resume_English.pdf" className="download-link">Click here to download</a>
-      <a href={pdfUrlFR} download="Diego_Rivera_Resume_French.pdf" className="download-link">Click here to download</a>      
+      <h4>Download My Resume</h4>
+      <a href={pdfUrlEN} download="Diego_Rivera_Resume_English.pdf" className="download-link"><FaFileDownload/>Resume (english)</a>
+      <a href={pdfUrlFR} download="Diego_Rivera_Resume_French.pdf" className="download-link"><FaFileDownload/>Resume (french)</a>      
         
 
     </div>
